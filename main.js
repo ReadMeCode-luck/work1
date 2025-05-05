5a6cdf21d21ba90649f41e766d87d6ff9d830cf0
ReadMeCode-luck <daniltokarev486@gmail.com>
2025-05-05 12:36:46 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+new Swiper('.swiper',{
+    loop:true,
+    slidesPerView: 1,
+    spaceBetween: 20,
+    navigation:{
+        prevEl:'.card__btn--prev',
+        nextEl:'.card__btnn--next',
+    }
+});
